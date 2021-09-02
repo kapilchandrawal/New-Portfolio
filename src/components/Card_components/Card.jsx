8419d27ec9049de3ce48d9adb987850b543c6c19
kapilchandrawal <77915124+kapilchandrawal@users.noreply.github.com>
2021-09-02 14:43:47 +0530

    Removed extra invidual cards

--- src/components/Card_components/Card.jsx
+++ src/components/Card_components/Card.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
-
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import { useStyles } from '../About/About_style.js';
-import js_img from '../../assets/javascript.png'
 
 
-export default function CSS_card() {
+export default function Cards(props) {
     const classes = useStyles();
-
     return (
         <div>
+
             <Card className={classes.root2}>
                 <CardContent className={classes.card}>
                     <CardMedia
                         className={classes.tech_image}
-                        image={js_img}
-                        title="Javascript"
+                        image={props.image}
+                        title={props.title}
                     />
                 </CardContent>
             </Card>
